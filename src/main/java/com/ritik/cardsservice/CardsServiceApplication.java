46776b543d7f38596abc3d602a76eb003c8288be
Ritik Ranjan <ritikranjan999@gmail.com>
2025-10-09 19:46:02 +0530

    Enable JPA auditing and add OpenAPI documentation.
    JPA auditing is enabled with a custom auditor implementation to streamline tracking of entity changes. OpenAPI annotations are introduced to provide detailed API documentation, including metadata like title, description, version, and contact information.

--- src/main/java/com/ritik/cardsservice/CardsServiceApplication.java
+++ src/main/java/com/ritik/cardsservice/CardsServiceApplication.java
@@ -2,8 +2,25 @@ package com.ritik.cardsservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+
 
 @SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Cards Service API",
+                description = "REST API Documentation for Cards Service",
+                version = "v1",
+                contact = @Contact(
+                        name = "Ritik Ranjan",
+                        email = "rranjan065.mca2023@nitrr.ac.in"
+                )
+        )
+)
 public class CardsServiceApplication {
 
     public static void main(String[] args) {
