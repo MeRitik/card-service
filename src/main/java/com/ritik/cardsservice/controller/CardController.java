82c5957cd64ed37124878f0540b190726834ff5d
Ritik Ranjan <ritikranjan999@gmail.com>
2026-02-18 00:32:06 +0530

    Implemented correlation-id logic for tracing

--- src/main/java/com/ritik/cardsservice/controller/CardController.java
+++ src/main/java/com/ritik/cardsservice/controller/CardController.java
@@ -14,6 +14,8 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Pattern;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
@@ -29,6 +31,8 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping(path = "/api", produces = "application/json")
 @Validated
 public class CardController {
+
+    private static final Logger log = LoggerFactory.getLogger(CardController.class);
 
     private final ICardService cardService;
     private final Environment environment;
@@ -94,9 +98,10 @@ public class CardController {
             )
     })
     @GetMapping("/fetch")
-    public ResponseEntity<CardDTO> fetchCardDetails(@RequestParam
-                                                    @Pattern(regexp = "(^$|[0-9]{10})", message = "Mobile number must be 10 digits")
-                                                    String mobileNumber) {
+    public ResponseEntity<CardDTO> fetchCardDetails(@RequestHeader(value = "bank-correlation-id") String correlationId, @RequestParam
+    @Pattern(regexp = "(^$|[0-9]{10})", message = "Mobile number must be 10 digits")
+    String mobileNumber) {
+        log.debug("bank-correlation-id found: {}", correlationId);
         CardDTO cardDto = cardService.fetchCard(mobileNumber);
         return ResponseEntity.status(HttpStatus.OK).body(cardDto);
     }
